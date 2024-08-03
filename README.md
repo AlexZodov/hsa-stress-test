c6ad8688ce798db8af7219f158883c177ac4287e
alexander.zodov <alexander.zodov@gen.tech>
2024-08-03 16:05:33 +0300

    feat(INIT): ADD basic app + docker
    -ADD description and proofs;

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # hsa-stress-test
+HSA homework 3 - stress test
+
+## How to start
+1. Clone the repo to local machine
+2. Head to `./server` on local machine and run `npm install` (required once to create node_modules folder before it will be projected to inner docker container filesystem)
+3. Head to root of the cloned repo
+4. Create actual `.env` file from `.env.dist` in the `./server` folder
+5. Run `docker-compose up -d`
+
+## How to test
+1. Install siege
+2. Provide/config it to accept given file `./siege/siege-urls.txt`
+3. Run siege with console command `siege -f ./siege/siege-urls.txt -c[concurrency_value] -r[requests_per_user_value] -b`
+4. Observe siege output and final summary
+
+## Proofs
+![](proofs/concurrency_10.png)
+![](./proofs/concurrency_25.png)
+![](./proofs/concurrency_50.png) 
+![](./proofs/concurrency_100.png) 
+![](./proofs/concurrency_200.png) 
+![](./proofs/concurrency_225.png) 
+![](./proofs/concurrency_255.png) 
---
+++ server/src/unit-1/ui/http/unit-1.controller.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateUnit1Dto, Unit1Dto, UpdateUnit1Dto } from '../dto';
+import {
+  CreateUnit1Handler,
+  GetUnit1Handler,
+  UpdateUnit1Handler,
+  UpdateUnit1Request,
+} from '../../operation';
+import * as uuid from 'uuid';
+
+@Controller('/unit-1')
+export class Unit1Controller {
+  constructor(
+    private readonly createUnit1Handler: CreateUnit1Handler,
+    private readonly updateUnit1Handler: UpdateUnit1Handler,
+    private readonly getUnit1Handler: GetUnit1Handler,
+  ) {}
+
+  @Post('/random')
+  async createRandomUnit1(@Body() dto: {type?: string}): Promise<Unit1Dto> {
+    const unit1 = await this.createUnit1Handler.handle({
+      unitId: uuid.v4(),
+      type: dto?.type ?? 'test',
+    });
+
+    return Unit1Dto.fromEntity(unit1);
+  }
+
+  @Post('/')
+  async createUnit1(@Body() dto: CreateUnit1Dto): Promise<Unit1Dto> {
+    const unit1 = await this.createUnit1Handler.handle({
+      unitId: dto.unit1_id,
+      type: dto.type,
+    });
+
+    return Unit1Dto.fromEntity(unit1);
+  }
+
+  @Get(':id')
+  async getUnit1ById(@Param('id') id: string): Promise<Unit1Dto> {
+    const unit1 = await this.getUnit1Handler.handle(id);
+
+    return Unit1Dto.fromEntity(unit1);
+  }
+
+  @Put('/:id')
+  async updateUnit1(
+    @Param('id') id: string,
+    @Body() dto: UpdateUnit1Dto,
+  ): Promise<Unit1Dto> {
+    const request = <UpdateUnit1Request>{ unitId: id, type: dto.type };
+    const unit1 = await this.updateUnit1Handler.handle(request);
+
+    return Unit1Dto.fromEntity(unit1);
+  }
+}
